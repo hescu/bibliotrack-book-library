cf2fd58c054c56ee18dea82a45f3a528859b4958
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-02-26 15:32:07 +0100

    Initial commit

---
+++ src/main/java/codingnomads/bibliotrackbooklibrary/BibliotrackBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package codingnomads.bibliotrackbooklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotrackBookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotrackBookLibraryApplication.class, args);
+	}
+
+}
