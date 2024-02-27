1477415982d821c7bab8ffaa68c83db621dffadc
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-02-27 15:01:31 +0100

    setup project and index

---
+++ src/main/java/codingnomads/bibliotrackbooklibrary/controller/AppController.java
@@ -0,0 +1,15 @@
+package codingnomads.bibliotrackbooklibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class AppController {
+
+    @GetMapping
+    public String getHomepage() {
+        return "index";
+    }
+}
