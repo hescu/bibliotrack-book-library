eb06c1043e6c405103dd6a49de692f6ad53af21b
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-03-07 17:00:27 +0100

    wip: implement search

--- src/main/java/codingnomads/bibliotrackbooklibrary/controller/AppController.java
+++ src/main/java/codingnomads/bibliotrackbooklibrary/controller/AppController.java
@@ -1,15 +1,46 @@
 package codingnomads.bibliotrackbooklibrary.controller;
 
+import codingnomads.bibliotrackbooklibrary.model.response.GoogleBooksApiResponse;
+import codingnomads.bibliotrackbooklibrary.model.response.SearchKeyword;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/")
 public class AppController {
 
+    @Value("${google.books.api.key}")
+    private String googleBooksApiKey;
+
     @GetMapping
-    public String getHomepage() {
+    public String displayIndex(Model model) {
+        model.addAttribute("searchKeyword", new SearchKeyword());
+        return "index";
+    }
+
+    @PostMapping("/search")
+    public String performSearch(@ModelAttribute("searchKeyword") SearchKeyword searchKeyword, Model model) throws JsonProcessingException {
+        String searchText = searchKeyword.getText();
+
+        // URL für die Google Books API-Anfrage
+        String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchText + "&key=" + googleBooksApiKey;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
+
+        // Konvertiere das JSON in ein GoogleBooksApiResponse-Objekt
+        ObjectMapper objectMapper = new ObjectMapper();
+        GoogleBooksApiResponse response = objectMapper.readValue(jsonResponse, GoogleBooksApiResponse.class);
+
+        model.addAttribute("searchResults", response.getItems());
         return "index";
     }
 }
---
+++ src/main/java/codingnomads/bibliotrackbooklibrary/model/response/SearchKeyword.java
@@ -0,0 +1,13 @@
+package codingnomads.bibliotrackbooklibrary.model.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SearchKeyword {
+
+    private String text;
+}
