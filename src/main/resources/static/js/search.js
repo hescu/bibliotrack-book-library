99f1ae9422aa36052378627013afbdfaeeb2c727
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-04-19 14:27:00 +0200

    change: db location

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,38 +1,21 @@
-$(document).ready(function() {
-    $('#addToWishlistButton').click(function() {
-        var formData = $('#addToWishlistForm').serialize();
-        $.ajax({
-            type: 'POST',
-            url: $('#addToWishlistForm').attr('action'),
-            data: formData,
-            success: function(response) {
-                showNotification(response, 'success');
-            },
-            error: function(xhr, status, error) {
-                var errorMessage = xhr.responseText || 'An error occurred.';
-                showNotification(errorMessage, 'error');
-            }
-        });
+ document.querySelectorAll('.addToWishlistButton').forEach(button => {
+        button.addEventListener('click', addToWishlist);
     });
-});
 
-function showNotification(message, type) {
-        var notificationDiv = $('#notification');
-        notificationDiv.removeClass(); // Remove any existing classes
-        notificationDiv.text(message); // Set the message text
+function addToWishlist(event) {
+    const isbn = event.target.dataset.isbn;
 
-        // Set the appropriate class for styling
-        if (type === 'success') {
-            notificationDiv.addClass('success');
-        } else if (type === 'error') {
-            notificationDiv.addClass('error');
+    fetch('/my-library/wishlist/add', isbn, {
+        method: 'POST'
+    })
+    .then(response => {
+        if (response.ok) {
+
+        } else {
+
         }
+    })
+    .catch(error => {
 
-        // Show the notification
-        notificationDiv.show();
-
-        // Hide the notification after a certain time (e.g., 5 seconds)
-        setTimeout(function() {
-            notificationDiv.hide();
-        }, 5000); // 5000 milliseconds = 5 seconds
-    }
+    });
+}
