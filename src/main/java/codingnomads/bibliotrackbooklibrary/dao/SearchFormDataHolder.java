5ce35a11b991c1fbbac005de91efaa9b657d4743
Hendrik Schuster <hendrik.schuster@consileon.de>
2024-07-03 10:49:05 +0200

    add: search controller tests

--- src/main/java/codingnomads/bibliotrackbooklibrary/dao/SearchFormDataHolder.java
+++ src/main/java/codingnomads/bibliotrackbooklibrary/dao/SearchFormDataHolder.java
@@ -1,13 +1,15 @@
 package codingnomads.bibliotrackbooklibrary.dao;
 
 import codingnomads.bibliotrackbooklibrary.model.forms.SearchFormData;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchFormDataHolder {
 
     private SearchFormData searchFormData;
